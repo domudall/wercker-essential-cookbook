5a55e4edf940df276e110c10c999c201e028771e
wwwouter <wouter@wercker.com>
2013-03-04 18:41:28 +0100

    version 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'pleasemailuse@wercker.com'
 license          'MIT'
 description      'Setup go'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
