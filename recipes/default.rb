0e7f0279c24dff378b4fa71755a149cebb7bf998
wwwouter <wouter@wercker.com>
2013-03-04 18:33:04 +0100

    added libs

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,20 +1,45 @@
-%w{wget build-essential curl git-core nano language-pack-en nodejs}.each do |pkg|
+%w{wget build-essential curl git-core nano language-pack-en nodejs erlang openjdk-6-jre ruby1.9.3 wget curl}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-%w{libxslt-dev libxml2-dev imagemagick libmagickwand-dev zlib1g-dev libfreetype6-dev liblcms1-dev libjpeg8-dev}.each do |pkg|
+%w{libxslt-dev libxml2-dev imagemagick libmagickwand-dev zlib1g-dev libfreetype6-dev liblcms1-dev libjpeg8-dev libevent-dev libev-dev libyaml-dev libffi-dev libtidy-dev libsqlite3-dev libcurl4-openssl-dev libssl-dev}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-%w{xvfb firefox}.each do |pkg|
+%w{memcached mongodb-clients}.each do |pkg|
   package pkg do
     action :install
   end
 end
+
+%w{memcached mongodb-clients}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
+%w{xvfb firefox libqtwebkit4 wkhtmltopdf}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
+script "install_phantomjs" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+    cd
+    wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.deb
+    dpkg -i elasticsearch-0.20.5.deb
+    EOH
+end
+
 
 if File.exists? "/vagrant"
   user = "vagrant"
